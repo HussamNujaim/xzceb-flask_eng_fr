b9ba8b0c272868b5224a3ae4a1d30009a319b7b1
Husam Nujaim <husam.nujaim@gmail.com>
2022-11-22 17:32:11 -0500

    tests

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,48 +1,40 @@
-import json 
-from ibm_watson import LanguageTranslatorV3, ApiException
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+"""
+This module does translation between French and English languages.
+"""
 import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
 
 load_dotenv()
+# set env parameters
+APIKEY = os.environ['apikey']
+URL = os.environ['url']
+VERSION = '2018-05-01'
 
-apikey = os.environ['apikey']
-url = os.environ['url']
-version = '2018-05-01'
-print(apikey)
-print(url)
-authenticator = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(version=version 
-                        ,authenticator=authenticator)
-language_translator.set_service_url(url)
+authenticator = IAMAuthenticator(APIKEY)
+language_translator = LanguageTranslatorV3(version=VERSION,
+authenticator=authenticator)
+language_translator.set_service_url(URL)
 
-def englishToFrench(englishText):
+def english_to_french(english_text):
     """
         Written by: Husam Nujaim
     """
+    if english_text is None:
+        return None
     translation = language_translator.translate(
-    text=englishText, model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
+    text = english_text, model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
     """
         Written by: Husam Nujaim
     """
+    if french_text is None:
+        return None
     translation = language_translator.translate(
-    text=frenchText, model_id='fr-en').get_result()
-    englishText = translation['translations'][0]['translation']
-    return englishText
-
-
-frenchText = englishToFrench("Hello, how are you today?")
-englishText = frenchToEnglish("Bonjour, comment vous êtes aujourd'hui?")
-print(5*'*')
-print('Translation EN = ', frenchText)
-print(5*'*')
-print('Translation FR = ', englishText)
-
-
-def frenchToEnglish(frenchText):
-    #write the code here
-    return englishText
+    text = french_text, model_id='fr-en').get_result()
+    english_text = translation['translations'][0]['translation']
+    return english_text
